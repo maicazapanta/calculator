54063f472ebdfb470f6e38a720aefca04eda1a77
maicazapanta <maicazapanta@github.com>
2019-07-09 19:01:13 +0800

    Created a components for buttons and result. And added some methods.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ResultComponent from './Components/ResultComponent';
+import KeyPadComponent from "./Components/KeyPadComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+      super();
+
+      this.state = {
+          result: ""
+      }
+  }
+
+  onClick = button =>{
+
+    if(button === "="){
+      this.handlesCalculate()
+    }
+
+    else if(button === "C")
+    {
+      this.handlesReset()
+    }
+
+    else if(button === "CE"){
+      this.handlesBackspace()
+    }
+
+    else{
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+  handlesCalculate = () =>{
+    try{
+      this.setState({
+        result: (eval(this.state.result) || "") + ""
+
+      })
+    }catch(e){
+      this.setState({
+        result: "error"
+        
+      })
+    }
+  };
+  handlesReset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  handlesBackspace = () =>{
+    this.setState({
+      result: this.state.slice(0, -1)
+    })
+  };
+
+
+
+  render() {
+      return (
+          <div>
+              <div className="calculator-body">
+                  <h1>Simple Calculator</h1>
+                  <ResultComponent result={this.state.result}/>
+                  <KeyPadComponent onClick={this.onClick}/>
+              </div>
+          </div>
+      );
+  }
 }
 
 export default App;
+
+
+
